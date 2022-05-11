339f9802beb61d865c921dafd9fe6c641e8ed691
mecaneer23 <mecaneer23@gmail.com>
2022-05-11 11:31:26 -0500

    refactor

--- slide_puzzle.py
+++ slide_puzzle.py
@@ -7,45 +7,39 @@ BOARD_SIZE = 3
 
 def main(stdscr):
     curses.use_default_colors()
-    board = ['  ']
-    board.extend(range(1, BOARD_SIZE * BOARD_SIZE))
-    board = [str(i).rjust(2, ' ') for i in board]
+    board = [str(i).rjust(2, ' ') for i in ["  ", *range(1, BOARD_SIZE * BOARD_SIZE)]]
     random.shuffle(board) # this leads to an occasional issue where the board is not solvable
     moves = 0
     stdscr.nodelay(0)
     while True:
         stdscr.clear()
-        stdscr.addstr(f"Moves: {moves}\n\n")
+        stdscr.addstr(f"Moves: {moves}\n\n    ")
         moves += 1
-        stdscr.addstr("    ")
-        counter = 1
-        for piece in board:
+        for i, piece in enumerate(board, 1):
             stdscr.addstr(f"{piece} ")
-            if counter % BOARD_SIZE == 0:
+            if i % BOARD_SIZE == 0:
                 stdscr.addstr("\n    ")
-            counter += 1
-        stdscr.addstr("\n")
-        stdscr.addstr("Move: ")
+        stdscr.addstr("\nMove: ")
         key = stdscr.getkey()
         if key == 'q':
-            return 'q'
+            return 'quit'
         move = key.rjust(2, ' ')
         open_space_loc = board.index('  ')
         try:
             move_loc = board.index(move)
         except ValueError:
             continue
-        if (
-            move_loc + 1 == open_space_loc
-            or move_loc - 1 == open_space_loc
-            or move_loc + BOARD_SIZE == open_space_loc
-            or move_loc - BOARD_SIZE == open_space_loc
+        if open_space_loc in (
+            move_loc + 1,
+            move_loc - 1,
+            move_loc + BOARD_SIZE,
+            move_loc - BOARD_SIZE
         ):
             board[open_space_loc], board[move_loc] = board[move_loc], board[open_space_loc]
         if board == sorted(board):
-            return 'w'
+            return 'win'
         stdscr.refresh()
 
 if __name__ == "__main__":
-    if curses.wrapper(main) == 'w':
+    if curses.wrapper(main) == 'win':
         print("You Win!")
