98c6081dd6f7c6952828463143d234c92f8f2c9e
mecaneer23 <mecaneer23@gmail.com>
2022-12-17 14:52:02 -0600

    doesn't work but the right idea

--- tk_slide_puzzle.py
+++ tk_slide_puzzle.py
@@ -2,66 +2,76 @@
 
 from tkinter import Tk, ttk, StringVar, messagebox
 import random
+BOARD_SIZE = 3
 
-BOARD_SIZE = 4
 
-
-def main():
-    def update_vars(board):
-        c = 0
-        for i in range(BOARD_SIZE):
-            for j in range(BOARD_SIZE):
-                vars[i][j].set(board[c])
-                c += 1
+def update(
+    board: list[str],
+    update_moves: bool,
+    variables: list[list[StringVar]],
+    moves: StringVar = StringVar(Tk(), "Moves: -1"),
+) -> None:
+    c = 0
+    for i in range(BOARD_SIZE):
+        for j in range(BOARD_SIZE):
+            variables[i][j].set(board[c])
+            c += 1
+    if update_moves:
         moves.set(f"Moves: {int(moves.get().split()[-1]) + 1}")
-        # print(
-        #     [[vars[j][i].get() for i in range(BOARD_SIZE)] for j in range(BOARD_SIZE)]
-        # )
 
-    def move(event=None, keycode=None):
-        nonlocal vars
-        if event is not None:
-            key = event.keycode
+
+def move(
+    event=None,
+    board: list = list(),
+    variables: list[list[StringVar]] = [[StringVar(Tk())]],
+    keycode: int | None = None,
+    update_counter: bool = True,
+) -> None:
+    if event is not None:
+        key = event.keycode
+    else:
+        key = keycode
+    open_space_loc = board.index(" ")
+    if key == 81:
+        root.destroy()
+        exit()
+    elif key in (87, 38):
+        if not (open_space_loc + BOARD_SIZE < BOARD_SIZE * BOARD_SIZE):
+            return
+        move_loc = open_space_loc + BOARD_SIZE
+    elif key in (83, 40):
+        if not (open_space_loc - BOARD_SIZE >= 0):
+            return
+        move_loc = open_space_loc - BOARD_SIZE
+    elif key in (65, 37):
+        if not (
+            (open_space_loc + 1) % BOARD_SIZE != 0
+            and open_space_loc + 1 < BOARD_SIZE * BOARD_SIZE
+        ):
+            return
+        move_loc = open_space_loc + 1
+    elif key in (68, 39):
+        if not (open_space_loc % BOARD_SIZE != 0 and open_space_loc - 1 >= 0):
+            return
+        move_loc = open_space_loc - 1
+    else:
+        return
+    board[open_space_loc], board[move_loc] = board[move_loc], board[open_space_loc]
+    update(board, update_counter, variables)
+    if board == sorted(board):
+        if messagebox.askyesno("You win!", "Do you want to play again?"):
+            root.destroy()
+
+            init()
         else:
-            key = keycode
-        open_space_loc = board.index(" ")
-        if key == 81:
             root.destroy()
             exit()
-        elif key in (87, 38):
-            if open_space_loc + BOARD_SIZE < BOARD_SIZE * BOARD_SIZE:
-                move_loc = open_space_loc + BOARD_SIZE
-            else:
-                return
-        elif key in (83, 40):
-            if open_space_loc - BOARD_SIZE >= 0:
-                move_loc = open_space_loc - BOARD_SIZE
-            else:
-                return
-        elif key in (65, 37):
-            if (
-                open_space_loc + 1
-            ) % BOARD_SIZE != 0 and open_space_loc + 1 < BOARD_SIZE * BOARD_SIZE:
-                move_loc = open_space_loc + 1
-            else:
-                return
-        elif key in (68, 39):
-            if open_space_loc % BOARD_SIZE != 0 and open_space_loc - 1 >= 0:
-                move_loc = open_space_loc - 1
-            else:
-                return
-        else:
-            return
-        board[open_space_loc], board[move_loc] = board[move_loc], board[open_space_loc]
-        update_vars(board)
-        if board == sorted(board):
-            if messagebox.askyesno("You win!", "Do you want to play again?"):
-                main()
 
+def init():
     root = Tk()
     root.title("Slide Puzzle")
-    root.bind_all("<Key>", move)
     board = [str(i) for i in [" ", *range(1, BOARD_SIZE * BOARD_SIZE)]]
+    root.bind_all("<Key>", lambda e: move(e, board))
     style = ttk.Style()
     style.configure(
         "Box.TLabel",
@@ -75,25 +85,29 @@ def main():
         "TLabel",
     )
     count = iter(board)
-    vars = [
+    variables = [
         [StringVar(root, next(count)) for _ in range(BOARD_SIZE)]
         for _ in range(BOARD_SIZE)
     ]
     [
         [
             ttk.Label(
-                root, textvariable=vars[i][j], padding=5, style=f"Box.TLabel"
+                root, textvariable=variables[i][j], padding=5, style=f"Box.TLabel"
             ).grid(row=i, column=j)
             for j in range(BOARD_SIZE)
         ]
         for i in range(BOARD_SIZE)
     ]
-    for _ in range(1000):
-        move(None, random.choice((37, 38, 39, 40)))
-        moves = StringVar(root, "Moves: -1")
-    ttk.Label(root, textvariable=moves).grid(columnspan=BOARD_SIZE, row=BOARD_SIZE + 1, column=0)
-    update_vars(board)
-    root.mainloop()
+    moves = StringVar(root)
+    for _ in range(10):
+        move(None, board, variables, random.choice((37, 38, 39, 40)), False)
+    moves.set("Moves: -1")
+    ttk.Label(root, textvariable=moves).grid(
+        columnspan=BOARD_SIZE, row=BOARD_SIZE + 1, column=0
+    )
+    update(board, True, variables)
+    return root, board, moves
 
 
-main()
+root, board, moves = init()
+root.mainloop()
